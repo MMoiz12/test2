37e325bc58c081b066028dc9216fd08c29f17022
aptech <moizaptech128@gmail.com>
2025-07-01 22:32:14 +0500

    'first-commit'

---
+++ app.js
@@ -0,0 +1,18 @@
+$("document").ready(() => {
+    $('.sidebar').hide();
+
+    $('button').click(()=>{
+        $('.sidebar').slideToggle()
+    })
+})
+
+
+
+// $('button').click(() => {
+
+//     $('#p').fadeToggle()
+
+
+// })
+
+
